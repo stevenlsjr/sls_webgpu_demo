45a1b49af1d54b31676696c8ea9969c28b7a565d
Steve Shea <stevenlsjr@gmail.com>
2021-08-15 12:31:55 -0400

    Move Handle types to separate source file

---
+++ src/renderer_common/handle.rs
@@ -0,0 +1,60 @@
+use std::marker::PhantomData;
+use std::ops::Deref;
+
+#[derive(Debug, Copy, Clone, Default, PartialEq)]
+pub struct HandleIndex(u32);
+
+const HANDLE_INDEX_N_BITS: u32 = 20;
+pub const HANDLE_INDEX_MASK: u32 = (1 << HANDLE_INDEX_N_BITS) - 1;
+const HANDLE_GENERATION_MASK: u32 = !HANDLE_INDEX_MASK;
+pub const GENERATION_MAX_SIZE: u32 = HANDLE_GENERATION_MASK >> HANDLE_INDEX_N_BITS;
+
+impl HandleIndex {
+  pub fn new(index: u32, generation: u16) -> Self {
+    Self(((generation as u32) << HANDLE_INDEX_N_BITS) | (index & HANDLE_INDEX_MASK))
+  }
+  pub fn index(&self) -> u32 {
+    self.0 & HANDLE_INDEX_MASK
+  }
+
+  pub fn generation(&self) -> u32 {
+    (self.0 & HANDLE_GENERATION_MASK) >> HANDLE_INDEX_N_BITS
+  }
+  pub(crate) fn into_typed<T>(self) -> Handle<T> {
+    Handle::from_index(index)
+  }
+}
+
+
+/// Typed wrapper for HandleIndex
+#[derive(Default, Debug, Copy, Clone, PartialEq)]
+pub struct Handle<T> {
+  index: HandleIndex,
+  _phantom: PhantomData<T>,
+}
+
+impl<T> Deref for Handle<T> {
+  type Target = HandleIndex;
+
+  fn deref(&self) -> &Self::Target {
+    &self.index
+  }
+}
+
+impl<T> Handle<T> {
+  pub fn from_index(index: HandleIndex) -> Self {
+    Self { index, _phantom: PhantomData }
+  }
+}
+
+
+
+pub trait ResourceStore<T>
+where T: Sized {
+  fn get_ref(&self, handle: Handle<T>) -> Option<&T>;
+  fn get_mut(&mut self, handle: Handle<T>) -> Option<&mut T>;
+
+  fn insert(&mut self, value: T) -> Handle<T>;
+
+  fn remove(&mut self, handle: T) -> Option<T>;
+}
